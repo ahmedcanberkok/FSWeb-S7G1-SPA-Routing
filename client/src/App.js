8462fe8bdbf8d2b8d03cc188c7bf33df357f2779
Canberk Ok <canberk.ok@gmail.com>
2023-05-01 23:38:01 +0300

    FilmListesi ve Film bileşenleri route ile eklendi.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
+import FilmListesi from './Filmler/FilmListesi';
+import Film from './Filmler/Film';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -13,7 +15,10 @@ export default function App () {
         .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
         .then(response => {
           // Bu kısmı log statementlarıyla çalışın
+          console.log('API yaniti: ',response);
           // ve burdan gelen response'u 'movieList' e aktarın
+          const movieList = response.data ;
+          setMovieList (movieList); // Burada movieList state'ine veriyi aktarıyoruz
         })
         .catch(error => {
           console.error('Sunucu Hatası', error);
@@ -24,13 +29,25 @@ export default function App () {
 
   const KaydedilenlerListesineEkle = id => {
     // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
+    if (!saved.includes(id)) {
+      setSaved([...saved, id]);
+    }
   };
 
   return (
+    <Router>
     <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
-
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
+      <KaydedilenlerListesi list={[ {saved}]} />
+      <Switch>
+        <Route exact path="/">
+          <FilmListesi movies= {movieList} />
+        </Route>
+        <Route path = "/filmler/:id">
+          <Film movies={movieList}/> 
+        </Route>
+      {/* <div>Bu Div'i kendi Routelarınızla değiştirin</div> */}
+      </Switch>
     </div>
-  );
+    </Router> 
+     );
 }
